295a3fe12a877fc6528321ea0e950198b0a27cc4
Bayastan <baastana7@gmail.com>
2025-02-19 00:25:25 +0600

    Первый коммит

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+"""
+Все объкты таблицы -- Post.objects.all() --> select - from posts
+
+один объект по условию -- Post.objects.get(id=1)
+
+объекты по условию не уникальному(несколько) -- Post.objects.filter(title='title')
+
+"""
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.CharField(max_length=1024)
+    rate = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.content}'
---
+++ posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, HttpResponse
+import random
+
+from django.template.context_processors import request
+
+from posts.models import Post
+
+
+def test_view(request):
+    return HttpResponse(f"Hello World {random.randint(1, 100)}")
+
+
+
+def html_view(request):
+    return render(request, "main.html")
+
+def post_list_view(request):
+    posts = Post.objects.all()
+    print(posts)
+    for post in posts:
+        print(post.title)
+    return render(request, 'posts/post_list.html', context={'posts': posts})
+
+def post_detail_view(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts/post_detail.html', context={'post': post})
